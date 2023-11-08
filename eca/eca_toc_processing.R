baeda633ac88142dae0005eb6ecbe823b6704512
smckever <68400750+smckever@users.noreply.github.com>
2023-11-07 17:37:01 -0800

    added mapping file

--- eca/eca_toc_processing.R
+++ eca/eca_toc_processing.R
@@ -9,13 +9,16 @@
 
 library(tidyverse)
 library(lubridate)
+library(readxl)
 
-
+mapping <- read_xlsx("C:/Users/guil098/OneDrive - PNNL/Data Generation and Files/ECA/CN/01_RawData/20230306_Mapping_SCN_ECA_EC_R1-169.xlsx")%>% 
+  mutate(Parent_ID = str_extract(Sample_ID, ".{5}(?=_)"))
 
 # RUN-1 ONLY --------------------------------------------------------------
 ## running just one replicate from each set for a general understanding of C values
 
-toc_run1 = read.csv("eca/toc_data/2023-02-21-stegen_eca_run1.csv") %>% janitor::clean_names()
+toc_run1 = read.csv("toc_data/2023-02-21-stegen_eca_run1.csv") %>%
+  janitor::clean_names()
 
 ## process toc data ----
 toc_processed = 
@@ -39,12 +42,20 @@ toc_samples =
   filter(!is.na(name)) %>% 
   dplyr::select(name, c_percent, n_percent, memo) %>% 
   rename(toc_percent = c_percent,
-         tn_percent = n_percent) %>% 
+         tn_percent = n_percent,
+         Randomized_ID = name) %>% 
   # some samples were re-run, so we want only the most recent ones
-  arrange(as.numeric(name))
+  arrange(as.numeric(Randomized_ID))
+
+#joining mapping with run 1
+toc_mapping <- mapping %>% 
+  full_join(toc_samples) %>% 
+  filter(!is.na(toc_percent)) %>% 
+  select(Sample_ID, Randomized_ID, 'CN Notes', Parent_ID, toc_percent, tn_percent, memo)
 
 ## export
 toc_samples %>% write.csv("eca/processed/toc_eca_run1_2023-03-06.csv", row.names = F)
+toc_mapping %>% write.csv("processed/toc_eca_run1_mapping.csv", row.names = F)
 
 # RUN-2 ONLY --------------------------------------------------------------
 ## running just one replicate from each set for a general understanding of C values
@@ -52,7 +63,8 @@ toc_samples %>% write.csv("eca/processed/toc_eca_run1_2023-03-06.csv", row.names
 toc_run2 = read.csv("toc_data/2023-02-21-stegen_eca_run1-contd.csv") %>% 
   janitor::clean_names() %>% 
   filter(!grepl("2/21/2023|2/22/2023|2/23/2023|2/24/2023", date_time)) 
-  
+
+
 
 ## process toc data ----
 toc_processed2 = 
