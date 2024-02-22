a7707ad0a127ef0a5f30ecda37fe9c175a8bd6c8
smckever <68400750+smckever@users.noreply.github.com>
2024-02-21 17:59:56 -0800

    processing new eca cn data

--- eca/eca_toc_processing.R
+++ eca/eca_toc_processing.R
@@ -11,8 +11,12 @@ library(tidyverse)
 library(lubridate)
 library(readxl)
 
-mapping <- read_xlsx("C:/Users/guil098/OneDrive - PNNL/Data Generation and Files/ECA/CN/01_RawData/20230306_Mapping_SCN_ECA_EC_R1-169.xlsx")%>% 
-  mutate(Parent_ID = str_extract(Sample_ID, ".{5}(?=_)"))
+
+# Sample Mapping --------------------------------------------------------------
+mapping <- read_xlsx("C:/Users/guil098/OneDrive - PNNL/Data Generation and Files/ECA/CN/01_RawData/20230628_Mapping_SCN_EC_EC_R1-288.xlsx")%>% 
+  janitor::clean_names() %>% 
+  mutate(parent_id = str_extract(sample_id, ".{5}(?=_)")) %>% 
+  filter(!is.na(sample_id))
 
 # RUN-1 ONLY --------------------------------------------------------------
 ## running just one replicate from each set for a general understanding of C values
@@ -93,4 +97,52 @@ toc_samples2 =
 
 ## export
 toc_samples2 %>% write.csv("processed/toc_eca_run2_2023-11-01.csv", row.names = F)
+
+
+
+# RUN-3 ONLY --------------------------------------------------------------
+## full run of batch 1 ECA samples 
+
+toc_run3 = read.csv("toc_data/2023-02-21-stegen_eca_run1-v3.csv") %>% 
+  janitor::clean_names()
+
+## process toc data ----
+toc_processed3 = 
+  toc_run3 %>% 
+  rename(n_percent = n,
+         c_percent = c) %>% 
+  dplyr::select(info, name, weight_mg, 
+                n_area, n_percent, n_factor, 
+                c_area, c_percent, c_factor, 
+                memo) %>% 
+  # remove samples with oxygen breakthrough ("bl") - these have been repeated
+  #filter(!info %in% "bl") %>% 
+  #filter(!memo %in% "skip") %>% 
+  force()
+
+## samples
+toc_samples3 = 
+  toc_processed3 %>% 
+  filter(!is.na(name),
+         !grepl("runin|RunIn|aspartic|MT|mt|moran-LOW|moran-MED|moran-low|moran-med", name)) %>% 
+  dplyr::select(name, c_percent, n_percent, memo) %>% 
+  rename(toc_percent = c_percent,
+         tn_percent = n_percent,
+         randomized_id = name) 
+
+toc_samples3$randomized_id = str_remove(toc_samples3$randomized_id, "EC_R|EC_|R")
+
+
+#joining mapping with full run
+toc_mapping <- mapping %>% 
+  full_join(toc_samples3) %>% 
+  filter(!is.na(toc_percent)) %>% 
+  select(sample_id, randomized_id, cn_notes, parent_id, toc_percent, tn_percent, memo)
+
+
+
+
+
+
+
 
