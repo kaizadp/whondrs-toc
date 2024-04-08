ded710ca9f5b6678737dd0f2b7be61b8c1108b3f
smckever <68400750+smckever@users.noreply.github.com>
2024-04-07 21:27:22 -0700

    updating code

--- eca/eca_toc_processing.R
+++ eca/eca_toc_processing.R
@@ -27,7 +27,8 @@ b2_mapping <- read_xlsx("C:/Users/guil098/OneDrive - PNNL/Data Generation and Fi
   mutate(parent_id = str_extract(sample_id, ".{6}(?=_)")) %>% 
   filter(!is.na(sample_id))
 
-full_mapping <- bind_rows(b1_mapping, b2_mapping)
+full_mapping <- bind_rows(b1_mapping, b2_mapping) %>% 
+  filter(!grepl("skip", cn_notes))
 
 # RUN-1 ONLY --------------------------------------------------------------
 ## running just one replicate from each set for a general understanding of C values
@@ -117,68 +118,51 @@ toc_samples2 %>% write.csv("processed/toc_eca_run2_2023-11-01.csv", row.names = 
 toc_run3 = read.csv("toc_data/2023-02-21-stegen_eca_run1-v3.csv") %>% 
   janitor::clean_names()
 
-toc_run4 = read.csv("toc_data/2023-11-07_stegen_eca_part2-v3.csv") %>% 
+toc_run4 = read.csv("toc_data/2023-11-07_stegen_eca_part2-v4.csv") %>% 
   janitor::clean_names()
 
+full_toc <- bind_rows(toc_run3, toc_run4)
+
 ## process toc data of batch 2 ----
-toc_processed3 = 
-  toc_run3 %>% 
+toc_processed = 
+  full_toc %>% 
   rename(n_percent = n,
          c_percent = c) %>% 
   dplyr::select(info, name, weight_mg, 
-                n_area, n_percent, n_factor, 
+                n_area, n_percent, n_factor,
                 c_area, c_percent, c_factor, 
                 memo) %>% 
   # remove samples with oxygen breakthrough ("bl") - these have been repeated
   #filter(!info %in% "bl") %>% 
-  filter(!memo %in% "skip") %>% 
+  #filter(!memo %in% "skip") %>% 
+  filter(!grepl("skip", memo)) %>% 
   force()
 
 ## samples
-toc_samples3 = 
-  toc_processed3 %>% 
+toc_samples = 
+  toc_processed %>% 
   filter(!is.na(name),
-         !grepl("runin|RunIn|aspartic|MT|mt|moran-LOW|moran-MED|moran-low|moran-med", name)) %>% 
+         !grepl("runin|aspartic|MT|moran", name, ignore.case = TRUE)) %>% 
   dplyr::select(name, c_percent, n_percent, memo) %>% 
   rename(toc_percent = c_percent,
          tn_percent = n_percent,
          randomized_id = name) 
-
-#toc_samples3$randomized_id = str_remove(toc_samples3$randomized_id, "EC_R|EC_|R")
-
-
-## process toc data of batch 1 ----
-toc_processed4 = 
-  toc_run4 %>% 
-  rename(n_percent = n,
-         c_percent = c) %>% 
-  dplyr::select(info, name, weight_mg, 
-                n_area, n_percent, n_factor, 
-                c_area, c_percent, c_factor, 
-                memo) %>% 
-  # remove samples with oxygen breakthrough ("bl") - these have been repeated
-  #filter(!info %in% "bl") %>% 
-  filter(!memo %in% "skip") %>% 
-  force()
-
-## samples
-toc_samples4 = 
-  toc_processed4 %>% 
-  filter(!is.na(name),
-         !grepl("runin|RunIn|aspartic|MT|mt|moran-LOW|moran-MED|moran-low|moran-med", name)) %>% 
-  dplyr::select(name, c_percent, n_percent, memo) %>% 
-  rename(toc_percent = c_percent,
-         tn_percent = n_percent,
-         randomized_id = name) 
-
-full_sample_toc <- bind_rows(toc_samples3, toc_samples4)
 
 #joining mapping with both runs
-toc_mapping <- full_mapping %>% 
-  full_join(full_sample_toc) %>% 
-  filter(!is.na(toc_percent)) %>% 
-  select(sample_id, randomized_id, cn_notes, parent_id, toc_percent, tn_percent, memo)
-
+toc_mapping <- 
+  toc_samples %>% 
+  left_join(full_mapping) %>% 
+  select(sample_id, randomized_id, cn_notes, parent_id, toc_percent, tn_percent, memo) %>% 
+  mutate(tn_percent = case_when(grepl("TC only", cn_notes) ~ NA, TRUE ~ tn_percent),
+         toc_percent = case_when(grepl("TN only", cn_notes) ~ NA, TRUE ~ toc_percent)) %>% 
+  dplyr::select(sample_id, parent_id, toc_percent, tn_percent) %>% 
+  pivot_longer(cols = c(toc_percent, tn_percent),
+               names_to = "Parameter",
+               values_to = "Percent") %>%
+  filter(!is.na(Percent)) %>%
+  filter(!is.na(sample_id)) %>% 
+  pivot_wider(names_from = Parameter,
+              values_from = Percent)
 
 # QAQC --------------------------------------------------------------
 full_processed_toc <- bind_rows(toc_processed3, toc_processed4)
@@ -218,29 +202,34 @@ blanks %>%
 ### 2. reps ----
 
 reps_c_analytical = 
-  full_processed_toc %>% 
-  dplyr::select(name, c_percent) %>% 
-  filter(grepl("rep", name)) %>% 
-  mutate(name = str_remove(name, "-rep")) %>% 
-  rename(rep = c_percent) %>% 
-  left_join(full_processed_toc %>% dplyr::select(name, c_percent)) %>% 
+  toc_mapping %>% 
+  dplyr::select(sample_id, toc_percent) %>% 
+  filter(grepl("rep", sample_id)) %>% 
+  mutate(sample_id = str_remove(sample_id, "-rep")) %>% 
+  rename(rep = toc_percent) %>% 
+  left_join(toc_mapping %>% dplyr::select(sample_id, toc_percent)) %>% 
   rowwise() %>% 
-  mutate(mean = mean(c(rep, c_percent)),
-         sd = round(sd(c(rep, c_percent)),3),
+  mutate(mean = mean(c(rep, toc_percent)),
+         sd = round(sd(c(rep, toc_percent)),3),
          cv = round(sd/mean,3))
 
 
 reps_n_analytical = 
-  full_processed_toc %>% 
-  dplyr::select(name, n_percent) %>% 
-  filter(grepl("rep", name)) %>% 
-  mutate(name = str_remove(name, "-rep")) %>% 
-  rename(rep = n_percent) %>% 
-  left_join(full_processed_toc %>% dplyr::select(name, n_percent)) %>% 
+  toc_mapping %>% 
+  dplyr::select(sample_id, tn_percent) %>% 
+  filter(grepl("rep", sample_id)) %>% 
+  mutate(sample_id = str_remove(sample_id, "-rep")) %>% 
+  rename(rep = tn_percent) %>% 
+  left_join(toc_mapping %>% dplyr::select(sample_id, tn_percent)) %>% 
   rowwise() %>% 
-  mutate(mean = mean(c(rep, n_percent)),
-         sd = round(sd(c(rep, n_percent)),3),
-         cv = round(sd/mean,3))
+  mutate(mean = mean(c(rep, tn_percent)),
+         sd = round(sd(c(rep, tn_percent)),3),
+         cv = round(sd/mean,3)) %>% 
+  mutate(parent_id = str_extract(sample_id, "[:digit:]+(?=_)"),
+         parent_id = case_when(nchar(parent_id) ==3 ~ parent_id, 
+                               nchar(parent_id) ==2 ~ str_c(0, parent_id)),
+         parent_id = paste0("EC_", parent_id)) 
+
 
 reps_c_analytical %>% 
   ggplot(aes(x = cv))+
@@ -259,7 +248,26 @@ reps_n_analytical %>%
   ggplot(aes(x = cv, y = mean))+
   geom_point()
 
+analytical_test <- toc_mapping %>% 
+  mutate(group_id = gsub("-rep$", "", sample_id))
 
+analytical_test_with_reps <- analytical_test %>% 
+  group_by(group_id) %>%
+  filter(n() > 1) %>%
+  ungroup()
+
+analytical_test_with_reps %>% 
+  ggplot(aes(x = sample_id, y = tn_percent, group = sample_id, color = sample_id)) +
+  geom_point(show.legend = FALSE) +
+  geom_text(data = reps_n_analytical, aes(label = paste("cv", round(cv, 2))), color = "black", hjust = 0.5) + 
+  labs(x = "Parent ID", y = "TN Percentage", title = "TN Percentage by Parent ID") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+        axis.text = element_text(size = 12),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_text(size = 14, face = "bold"))
+
+##make a bar graph of the reps with error bars
 
 ### 3. aspartics/standards ----
 aspartics = 
@@ -284,8 +292,7 @@ aspartics %>%
 reps_c_by_site = 
   toc_mapping %>% 
   mutate(treat = case_when(grepl("W",sample_id)~"Wet",
-                            grepl("D", sample_id) ~"Dry")) %>% 
-  filter(!grepl("rerunning for TN only|power bump", cn_notes)) %>% 
+                            grepl("D", sample_id) ~"Dry")) %>%
   group_by(parent_id, treat) %>% 
   summarise(mean = mean(toc_percent),
             sd = round(sd(toc_percent),3),
@@ -297,7 +304,9 @@ reps_c_by_site %>%
 
 reps_n_by_site = 
   toc_mapping  %>% 
-  group_by(parent_id) %>% 
+  mutate(treat = case_when(grepl("W",sample_id)~"Wet",
+                           grepl("D", sample_id) ~"Dry")) %>% 
+  group_by(parent_id, treat) %>% 
   summarise(mean = mean(tn_percent),
             sd = round(sd(tn_percent),3),
             cv = round(sd/mean,3))
@@ -305,4 +314,54 @@ reps_n_by_site =
 reps_n_by_site %>% 
   ggplot(aes(x = cv))+
   geom_histogram()
+
+
+
+
+highlighted_parent_ids <- c("EC_042", "EC_069", "EC_052", "EC_089", "EC_094", 
+                            "EC_091", "EC_093", "EC_073", "EC_065", "EC_051", 
+                            "EC_090", "EC_076", "EC_009", "EC_082", "EC_075", 
+                            "EC_071", "EC_095", "EC_080", "EC_084", 
+                            "EC_081", "EC_017", "EC_066", "EC_021", "EC_024", 
+                            "EC_038", "EC_022", "EC_064", "EC_039", "EC_034", 
+                            "EC_035", "EC_041", "EC_013", "EC_032")
+
+
+tn_distribution <- ggplot(data = toc_mapping, aes(x = tn_percent)) +
+  geom_histogram(binwidth = 0.05, fill = "lightblue", color = "black", alpha = 0.7) +
+  geom_vline(data = toc_mapping[toc_mapping$parent_id %in% highlighted_parent_ids, ],
+             aes(xintercept = tn_percent, color = parent_id), 
+             linetype = "dashed", size = 0.8) +
+  scale_color_manual(name = "Parent ID",
+                     values = c("EC_042" = "#FF0000", "EC_069" = "#0000FF", "EC_052" = "#008000",
+                               "EC_089" = "#800080", "EC_094" = "#FFA500", "EC_091" = "#FFC0CB",
+                               "EC_093" = "#A52A2A", "EC_073" = "#00FFFF", "EC_065" = "#808080",
+                               "EC_051" = "#EE82EE", "EC_090" = "#FFFF00", "EC_076" = "#006400",
+                               "EC_009" = "#FA8072", "EC_082" = "#87CEEB", "EC_075" = "#FF00FF",
+                               "EC_071" = "#DA70D6", "EC_095" = "#FFD700", "EC_080" = "#FF8C00",
+                               "EC_084" = "#00008B", "EC_081" = "#32CD32", "EC_017" = "#008080",
+                               "EC_066" = "#40E0D0", "EC_021" = "#808000", "EC_024" = "#A0522D",
+                               "EC_038" = "#CD853F", "EC_022" = "#708090", "EC_064" = "#FF6347",
+                               "EC_039" = "#DAA520", "EC_034" = "#6495ED", "EC_035" = "#F08080",
+                               "EC_041" = "#FFA07A", "EC_013" = "#90EE90", "EC_032" = "#FFB6C1")) +
+  labs(x = "TN Percent", y = "Frequency", title = "Distribution of TN Percent") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"), 
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14, face = "bold"))
+
+library(viridis)
+
+tn <- ggplot(data = toc_mapping, aes(x = tn_percent)) +
+  geom_histogram(binwidth = 0.05, fill = "lightblue", color = "black", alpha = 0.7) +
+  geom_point(aes(x = tn_percent, y = 0), size = 2)+
+  # geom_segment(data = toc_mapping[toc_mapping$parent_id %in% highlighted_parent_ids, ],
+  #              aes(x = tn_percent, xend = tn_percent, y = 0, yend = 50, color = parent_id), 
+  #              linetype = "dashed", size = 0.8) +
+  scale_color_manual(name = "Parent ID", values = viridis(length(highlighted_parent_ids))) +
+  labs(x = "TN Percent", y = "Frequency", title = "Distribution of TN Percent") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"), 
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14, face = "bold"))
 
---
+++ icon/cm_toc_processing.R
@@ -0,0 +1,336 @@
+## WHONDRS ICON
+## TOC PROCESSING
+## 
+## Kaizad F. Patel & Sophia McKever
+## March 2024
+
+#################### #
+#################### #
+
+library(tidyverse)
+library(readxl)
+library(viridis)
+
+
+
+#ICON RUN --------------------------------------------------------------
+
+toc_cm = read.csv("toc/2024-02-16-sm-cm.csv") %>% 
+  janitor::clean_names() %>% 
+  filter(!grepl("2/23/2024|2/26/2024", date_time))
+
+mapping = read_xlsx("C:/Users/guil098/OneDrive - PNNL/Data Generation and Files/ICON_ModEx_SSS/08_CN/01_RawData/20230524_Data_Raw_SCN_SBR_RC4_CM_R1-89/20230524_Mapping_Raw_SCN_SBR_RC4_CM_R1-129.xlsx") %>% 
+  janitor::clean_names() %>% 
+  mutate(parent_id = str_extract(sample_id, ".{6}(?=_)")) %>% 
+  filter(!is.na(sample_id))
+
+
+## process toc data ----
+toc_processed = 
+  toc_cm %>% 
+   rename(n_percent = n,
+          c_percent = c) %>% 
+  dplyr::select(info, name, weight_mg, 
+                n_area, n_percent, n_factor, 
+                c_area, c_percent, c_factor, 
+                memo) %>% 
+  # remove samples with oxygen breakthrough ("bl") - these have been repeated
+  #filter(!info %in% "bl") %>% 
+  #filter(!memo %in% "skip") %>% 
+  force()
+
+## samples
+
+toc_samples = 
+  toc_processed %>% 
+  filter(grepl("CM", name),
+         !grepl("-rep",name)) %>% 
+  dplyr::select(name, c_percent, n_percent, memo) %>% 
+  rename(toc_percent = c_percent,
+         tn_percent = n_percent,
+         randomized_id = name) %>% 
+  left_join(mapping, by = c('randomized_id')) %>% 
+  select(sample_id, toc_percent, tn_percent, randomized_id, parent_id)
+
+
+toc_samples_singles =
+  toc_samples %>% 
+  group_by(parent_id) %>% 
+  filter(n()== 1) %>% 
+  select(c(parent_id, toc_percent, tn_percent)) %>% 
+  mutate(Method_Deviation = "N/A")
+
+
+# QA-QC --------------------------------------------------------------
+
+### 1. blanks ----
+blanks = 
+  toc_processed %>% 
+  dplyr::select(name, c_area, c_percent, n_area, n_percent, memo) %>% 
+  filter(grepl("MT", name, ignore.case = TRUE)) 
+
+# blanks for C
+blanks %>% 
+  ggplot(aes(x = "blank", y = c_percent)) + 
+  geom_violin()+
+  geom_jitter(width = 0.1)
+
+blanks %>% 
+  ggplot(aes(x = "blank", y = c_area)) + 
+  geom_violin()+
+  geom_jitter(width = 0.1)
+
+
+# blanks for N
+blanks %>% 
+  filter(n_area < 2000) %>% 
+  ggplot(aes(x = "blank", y = n_percent)) + 
+  geom_violin()+
+  geom_jitter(width = 0.1)
+
+blanks %>% 
+  filter(n_area < 2000) %>% 
+  ggplot(aes(x = "blank", y = n_area)) + 
+  geom_violin()+
+  geom_jitter(width = 0.1)
+
+
+### 2. reps ----
+
+reps_c_analytical = 
+  toc_processed %>% 
+  dplyr::select(name, c_percent) %>% 
+  filter(grepl("rep", name)) %>% 
+  mutate(name = str_remove(name, "-rep")) %>% 
+  rename(rep = c_percent) %>% 
+  left_join(toc_processed %>% dplyr::select(name, c_percent)) %>% 
+  rowwise() %>% 
+  mutate(mean = mean(c(rep, c_percent)),
+         sd = round(sd(c(rep, c_percent)),3),
+         cv = round(sd/mean,3))
+
+
+reps_n_analytical = 
+  toc_processed %>% 
+  dplyr::select(name, n_percent) %>% 
+  filter(grepl("rep", name)) %>% 
+  mutate(name = str_remove(name, "-rep")) %>% 
+  rename(rep = n_percent) %>% 
+  left_join(toc_processed %>% dplyr::select(name, n_percent)) %>% 
+  rowwise() %>% 
+  mutate(mean = mean(c(rep, n_percent)),
+         sd = round(sd(c(rep, n_percent)),3),
+         cv = round(sd/mean,3))
+
+reps_c_analytical %>% 
+  ggplot(aes(x = cv))+
+  geom_histogram()
+
+reps_n_analytical %>% 
+  ggplot(aes(x = cv))+
+  geom_histogram()
+
+
+### 3. aspartics/standards ----
+aspartics = 
+  toc_processed %>% 
+  filter(name == "aspartic")
+
+aspartics %>% 
+  filter(weight_mg > 0.4 & weight_mg < 0.65) %>% 
+  ggplot(aes(y = c_factor, x = "aspartic"))+
+  geom_violin()+
+  geom_jitter(width = 0.2)
+
+aspartics %>% 
+  filter(weight_mg > 0.4 & weight_mg < 0.65) %>% 
+  ggplot(aes(y = n_factor, x = "aspartic"))+
+  geom_violin()+
+  geom_jitter(width = 0.2)
+
+
+### 4. reps by site ----
+
+reps_c_by_site = 
+  toc_samples %>% 
+  group_by(parent_id) %>% 
+  summarise(mean = mean(toc_percent),
+            sd = round(sd(toc_percent),3),
+            cv = round(sd/mean,3))
+
+reps_c_by_site %>% 
+  ggplot(aes(x = cv))+
+  geom_histogram()
+
+reps_n_by_site = 
+  toc_samples %>% 
+  group_by(parent_id) %>% 
+  summarise(mean = mean(tn_percent),
+            sd = round(sd(tn_percent),3),
+            cv = round(sd/mean,3))
+
+reps_n_by_site %>% 
+  ggplot(aes(x = cv))+
+  geom_histogram()
+
+# Maggi Dist Matrix  --------------------------------------------------------------
+
+#calculate mean and cv for replicate samples
+outliers <- toc_samples %>% 
+  group_by(parent_id) %>% 
+  mutate(mean_toc_all = mean(toc_percent)) %>% 
+  mutate(cv_toc_all = (sd(toc_percent)/mean(toc_percent))*100) %>% 
+  filter(n() > 1) %>% 
+  ungroup() 
+
+outliers$flag <- NA
+
+#make final data frame
+toc_final <- as.data.frame(matrix(NA, ncol = 10, nrow = 1))
+
+colnames(toc_final) = c("toc.temp", "sample_id",  "tn_percent", "randomized_id", "parent_id", "mean_toc_all", "cv_toc_all", "mean_toc_rem", "cv_toc_rem", "flag")
+
+#Number of unique parent IDs with replicates to loop through
+unique.samples = unique(outliers$parent_id)
+
+for (i in 1:length(unique.samples)) {
+  
+  ## Subset replicates
+  data_subset = subset(outliers, outliers$parent_id == unique.samples[i])
+  
+  ## Pull out toc percent values
+  toc.temp = as.numeric(data_subset$toc_percent)
+  
+  ## Calculate standard deviation, average, and coefficient of variation of rates
+  toc.temp.sd <- sd(toc.temp)
+  toc.temp.mean <- mean(toc.temp)
+  CV = abs((toc.temp.sd/toc.temp.mean)*100)
+  
+  #looping to get 3 best samples
+  for (sample.reduction in 1:5)  {
+    
+    if (length(toc.temp) > 3) {
+      
+      dist.temp = as.matrix(abs(dist(toc.temp)))
+      dist.comp = numeric() 
+      
+      for(toc.now in 1:ncol(dist.temp)) {
+        
+        dist.comp = rbind(dist.comp,c(toc.now,sum(dist.temp[,toc.now])))
+        
+      }
+      
+      dist.comp[,2] = as.numeric(dist.comp[,2])
+      toc.temp = toc.temp[-which.max(dist.comp[,2])]
+      
+      toc.temp.sd <- sd(toc.temp)
+      toc.temp.mean <- mean(toc.temp)
+      toc.temp.cv <- abs((toc.temp.sd/toc.temp.mean)*100)
+      CV = toc.temp.cv
+      toc.temp.range <- max(toc.temp) - min(toc.temp)
+      range = toc.temp.range
+      
+    } 
+  }
+  
+  if (length(toc.temp) >= 3) {
+    
+    toc.combined <- as.data.frame(toc.temp)
+    
+    toc.removed <- merge(toc.combined, data_subset, by.x = "toc.temp", by.y = "toc_percent", all.x = TRUE)
+    
+    toc.removed <- toc.removed[!duplicated(toc.removed$Sample_ID), ]
+    
+    toc.removed$cv_toc_rem = as.numeric(abs((sd(toc.temp)/mean(toc.temp))*100))
+    
+    toc.removed$mean_toc_rem = as.numeric(mean(toc.temp))
+    
+  }
+  
+  toc_final = rbind(toc.removed, toc_final)
+  
+  rm('toc.temp')
+}
+
+## This data frame has removed samples
+toc_final_flag <- toc_final %>% 
+  mutate(flag = if_else(cv_toc_all < cv_toc_rem, "Issue in dropping sample", "N/A")) %>% 
+  rename(toc_percent = toc.temp) 
+
+### End Sample Removal ####
+
+## Joined to see what sample is being removed
+
+toc_merged_removals <- left_join(outliers, toc_final_flag, by = c("Sample_ID", "Randomized_ID", "tn_percent", "parent_id", "mean_toc_all", "cv_toc_all")) %>% 
+  mutate(flag_removal = if_else(is.na(toc_percent.y), "Outlier to remove", "N/A")) %>% 
+  left_join(toc_samples, by = c("Sample_ID", "Randomized_ID", "tn_percent", "parent_id")) %>% 
+  mutate(toc_percent = ifelse(flag_removal == "Outlier to remove", "N/A", toc_percent.x)) %>% 
+  select(-c(flag.x, flag.y, toc_percent.x, toc_percent.y)) %>% 
+  relocate(toc_percent, .after = Sample_ID)
+
+
+clean_replicates <- 
+  toc_merged_removals %>% 
+  filter(flag_removal == "N/A") %>% 
+  group_by(parent_id) %>% 
+  mutate(toc_percent = as.numeric(toc_percent)) %>% 
+  dplyr::summarise(toc_percent = mean(toc_percent),
+                   tn_percent = mean(tn_percent),
+                   toc_percent = round(toc_percent, 2),
+                   tn_percent = round(tn_percent, 2),
+                   n = n()) %>% 
+  mutate(Method_Deviation = case_when(n >= 3 ~ "CN_000", n == 1 ~ "N/A")) %>% 
+  select(c(parent_id, toc_percent, tn_percent, Method_Deviation))
+
+toc_final_samples <- 
+  rbind(clean_replicates, toc_samples_singles)
+
+toc_final_samples$parent_id <- paste0(toc_final_samples$parent_id,"_SCN")
+
+
+
+
+
+
+#Data Package ----
+#ICON Data Package set
+
+toc_dp <- toc_final_samples %>% 
+  select(parent_id, toc_percent, tn_percent, Method_Deviation) %>% 
+  mutate(Material = "Sediment") %>% 
+  relocate(Material, .after = parent_id) %>% 
+  rename(Sample_Name = parent_id,
+         '01395_C_percent_per_mg' = toc_percent,
+         '01397_N_percent_per_mg' = tn_percent,
+         Methods_Deviation = Method_Deviation) 
+  
+#formatted for data package file 
+toc_dp %>% write.csv("C:/Users/guil098/OneDrive - PNNL/Data Generation and Files/ICON_ModEx_SSS/08_CN/02_FormattedData/SSS_CN_ReadyForBoye_01-12-2024.csv", row.names = F)  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
